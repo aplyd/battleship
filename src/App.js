b3b3ca3fa906a9def96b0c73c9ba8d09f0cf6e4e
aplyd <austin@ftacnik.com>
2020-03-16 15:42:13 -0700

    prettier config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import './App.css'
+import "./App.css";
 
-import Gameboard from './components/Gameboard'
-import StartGameModal from './components/StartGameModal';
+import Gameboard from "./components/Gameboard";
+import StartGameModal from "./components/StartGameModal";
 
-import { generateComputerBoard, 
-        generateBoard, 
-        generateComputerAttacks, 
-        getCoordinate,
-        getIndex,
-        getSurroundingSpaces } from './gameEngine';
+import {
+  generateComputerBoard,
+  generateBoard,
+  generateComputerAttacks,
+  getCoordinate,
+  getIndex,
+  getSurroundingSpaces
+} from "./gameEngine";
 
 //TODO - refactor with "state machine"
 //TODO - confirm empty spaces (ships cant touch, so surrounding areas empty if ship sinks)
@@ -20,7 +22,7 @@ export class App extends Component {
     super();
     this.state = {
       isModalOpen: true,
-      username: '',
+      username: "",
       user: null,
       computer: null,
       computerAttacks: [],
@@ -28,87 +30,86 @@ export class App extends Component {
       allShipsPlaced: false,
       placedShipCounter: 0,
       usersTurn: false,
-      attackCounter: 0,
-    }
+      attackCounter: 0
+    };
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.placedShipCounter === 19) {
       setTimeout(() => {
-        this.setState({ 
-          allShipsPlaced: true, 
+        this.setState({
+          allShipsPlaced: true,
           usersTurn: true
-      })}, 2000)
+        });
+      }, 2000);
     }
   }
 
   UNSAFE_componentWillMount = () => {
     this.getInitialState();
-  }
-
+  };
 
   getInitialState = () => {
     const user = generateBoard();
     const computer = generateComputerBoard();
     const computerAttacks = generateComputerAttacks();
     this.setState({ user, computer, computerAttacks });
-  }
-  
-  getUserName = (name) => {
-    this.setState({ 
-      username: name,
-    })
-    this.toggleStartGameModal()
-  }
+  };
+
+  getUserName = name => {
+    this.setState({
+      username: name
+    });
+    this.toggleStartGameModal();
+  };
 
   toggleStartGameModal = () => {
-    this.setState({ isModalOpen: !this.state.isModalOpen})
-  }
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
 
   handleSpaceClick = (e, index) => {
-    const user = [...this.state.user]
+    const user = [...this.state.user];
 
     //placing the ships until all 20 are placed
     if (this.state.placedShipCounter <= 19 && user[index][0] === false) {
       user[index][0] = true;
       this.setState({
         user,
-        placedShipCounter: this.state.placedShipCounter + 1,
-      })
+        placedShipCounter: this.state.placedShipCounter + 1
+      });
     }
-  }
-  
+  };
+
   handleAttack = (e, index) => {
-    //using attack counter to ensure only 1 attack per turn 
-    this.setState({ attackCounter:  this.state.attackCounter + 1})
+    //using attack counter to ensure only 1 attack per turn
+    this.setState({ attackCounter: this.state.attackCounter + 1 });
     if (this.state.attackCounter < 1) {
       //copy computer board because thats what were attacking
-      const computer = [...this.state.computer]
+      const computer = [...this.state.computer];
 
       //computer[index][0] = occupied by ship, [1] = has been damaged
       //if attack hits ship, allow for another turn
       if (computer[index][0] === true) {
         computer[index][1] = true;
-          this.setState({
-            computer
-          })
+        this.setState({
+          computer
+        });
 
-          this.setState({ attackCounter: 0 })
-      //if not, dont allow for another turn
+        this.setState({ attackCounter: 0 });
+        //if not, dont allow for another turn
       } else {
         computer[index][1] = true;
-          this.setState({
-            computer
-          })
+        this.setState({
+          computer
+        });
 
         setTimeout(() => {
-          this.setState({ usersTurn: false, attackCounter: 0 })
+          this.setState({ usersTurn: false, attackCounter: 0 });
           this.computerAttack();
-        }, 3000)
+        }, 3000);
       }
-      
     }
-  }
+  };
 
   computerAttack = () => {
     //copy user board because thats what computer is attacking
@@ -120,50 +121,46 @@ export class App extends Component {
     if (user[attackArr[index]][0] === true) {
       setTimeout(() => {
         user[attackArr[index]][1] = true;
-          this.setState({ 
-            user,
-            computerAttackCounter: index + 1 
-          })
-          this.computerAttack();
-        }, 1500)
-
-        
+        this.setState({
+          user,
+          computerAttackCounter: index + 1
+        });
+        this.computerAttack();
+      }, 1500);
     } else {
       setTimeout(() => {
         user[attackArr[index]][1] = true;
-          this.setState({ 
-            user,
-            computerAttackCounter: index + 1 
-          })
-        }, 1500)
-      
-        setTimeout(() => {
-            this.setState({ usersTurn: true })
-        }, 3000)
+        this.setState({
+          user,
+          computerAttackCounter: index + 1
+        });
+      }, 1500);
+
+      setTimeout(() => {
+        this.setState({ usersTurn: true });
+      }, 3000);
     }
-    
-  }
+  };
 
   render() {
     return (
       <React.Fragment>
-        {this.state.isModalOpen 
-        ? <StartGameModal getUserName={this.getUserName} /> 
-        : null}
+        {this.state.isModalOpen ? (
+          <StartGameModal getUserName={this.getUserName} />
+        ) : null}
 
         <div id="gameboard-container">
-          <Gameboard user={this.state.user} 
-                    handleSpaceClick={this.handleSpaceClick} 
-                    computer={this.state.computer} 
-                    usersTurn={this.state.usersTurn}
-                    handleAttack={this.handleAttack}
-                    />
+          <Gameboard
+            user={this.state.user}
+            handleSpaceClick={this.handleSpaceClick}
+            computer={this.state.computer}
+            usersTurn={this.state.usersTurn}
+            handleAttack={this.handleAttack}
+          />
         </div>
-        
-
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/gameEngine.js
+++ src/gameEngine.js
@@ -6,122 +6,132 @@
 //TODO - function to translate 0-99 to a-j, 0-9 cordinates. then use that for ship placement.
 
 export function generateBoard() {
-    let arr = []
-    for (let i = 0; i < 100; i++) {
-        arr.push([false, false])
-    }
-    
-    return arr
+	let arr = [];
+	for (let i = 0; i < 100; i++) {
+		arr.push([false, false]);
+	}
+
+	return arr;
 }
 
-const generateRandomNum = (n) => Math.floor(Math.random() * n)
+const generateRandomNum = n => Math.floor(Math.random() * n);
 
-export const generateUniqueNums = (n) => {
-    const arr = []
+export const generateUniqueNums = n => {
+	const arr = [];
 
-    while (arr.length < 20) {
-        let num = generateRandomNum(n)
-        if (!arr.includes(num)) {
-            arr.push(num)
-        }
-    }
+	while (arr.length < 20) {
+		let num = generateRandomNum(n);
+		if (!arr.includes(num)) {
+			arr.push(num);
+		}
+	}
 
-    return arr
-}
+	return arr;
+};
 
 //ships
 //1.length x4 | 2.length x3 | 3.length x2 | 4.length x1
 
 export const getSurroundingSpaces = (coordinate = 'c6') => {
-    //instead of just add/sub from the index of coordinate, using letters helps be aware of edge of grid;
-    //the x's are there for fallback. if the returned coordinate is 'x6', its not on the grid. 
-    //not necesarry for nums because -1 or 11 aren't on grid either
-    const letters = ['x', 'x', 'x', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'x', 'x', 'x'];
-    const letter = coordinate[0];
-    const num = Number(coordinate[1]);
-    let arr = [];
-    const letterIndex = letters.indexOf(letter);
+	//instead of just add/sub from the index of coordinate, using letters helps be aware of edge of grid;
+	//the x's are there for fallback. if the returned coordinate is 'x6', its not on the grid.
+	//not necesarry for nums because -1 or 11 aren't on grid either
+	const letters = [
+		'x',
+		'x',
+		'x',
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'x',
+		'x',
+		'x'
+	];
+	const letter = coordinate[0];
+	const num = Number(coordinate[1]);
+	let arr = [];
+	const letterIndex = letters.indexOf(letter);
 
-    //god this is ugly
-    arr.push((letters[letterIndex - 1] + (num - 1)));
-    arr.push((letters[letterIndex] + (num - 1)));
-    arr.push((letters[letterIndex + 1] + (num - 1)));
-    arr.push((letters[letterIndex - 1] + num));
-    arr.push((letters[letterIndex + 1] + num));
-    arr.push((letters[letterIndex - 1] + (num + 1)));
-    arr.push((letters[letterIndex] + (num + 1)));
-    arr.push((letters[letterIndex + 1] + (num + 1)));
+	//god this is ugly
+	arr.push(letters[letterIndex - 1] + (num - 1));
+	arr.push(letters[letterIndex] + (num - 1));
+	arr.push(letters[letterIndex + 1] + (num - 1));
+	arr.push(letters[letterIndex - 1] + num);
+	arr.push(letters[letterIndex + 1] + num);
+	arr.push(letters[letterIndex - 1] + (num + 1));
+	arr.push(letters[letterIndex] + (num + 1));
+	arr.push(letters[letterIndex + 1] + (num + 1));
 
-    return arr;
-}
+	return arr;
+};
 
 const shipDirection = () => {
-    const arr = ['up', 'down', 'right', 'left'];
+	const arr = ['up', 'down', 'right', 'left'];
+};
 
+const positionShip = direction => {
+	//increment number = down
+	//decrement number = up
+	//increment letter = right
+	//decrement letter = left
+	if (direction === 'up') {
+	}
 
-}
+	if (direction === 'down') {
+	}
 
-const positionShip = (direction) => {
-    //increment number = down
-    //decrement number = up
-    //increment letter = right
-    //decrement letter = left
-    if (direction === 'up') {
-
-    }
-
-    if (direction === 'down') {
-
-    }
-
-    if (direction === 'right') {
-
-    }
-
-}
+	if (direction === 'right') {
+	}
+};
 
 export function generateComputerBoard() {
-    let board = generateBoard();
-    let arr = generateUniqueNums(20);
+	let board = generateBoard();
+	let arr = generateUniqueNums(20);
 
-    arr.forEach((num) => {
-        board[num][0] = true;
-    })
-    
-    return board;
+	arr.forEach(num => {
+		board[num][0] = true;
+	});
+
+	return board;
 }
 
-export function generateComputerAttacks(difficulty = 'easy') { 
-    let arr = [];
+export function generateComputerAttacks(difficulty = 'easy') {
+	let arr = [];
 
-    if (difficulty === 'easy') {
-        arr = generateUniqueNums(100);
-    }
+	if (difficulty === 'easy') {
+		arr = generateUniqueNums(100);
+	}
 
-    return arr;
+	return arr;
 }
 
 export function coordinatesArr() {
-    const arr = [...Array(100)];
-    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    let num;
+	const arr = [...Array(100)];
+	const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+	let num;
 
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            num = i + 1;
-            arr[Number("" + i + j)] = letters[j] + num.toString();
-        }
-    }
-    return arr
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			num = i + 1;
+			arr[Number('' + i + j)] = letters[j] + num.toString();
+		}
+	}
+	return arr;
 }
 
-export const getCoordinate = (index) => {
-    const arr = coordinatesArr();
+export const getCoordinate = index => {
+	const arr = coordinatesArr();
 
-    return arr[index];
-}
+	return arr[index];
+};
 
 export const getIndex = (coordinate = 'c6') => {
-
-    return coordinatesArr().indexOf(coordinate);
-}
+	return coordinatesArr().indexOf(coordinate);
+};

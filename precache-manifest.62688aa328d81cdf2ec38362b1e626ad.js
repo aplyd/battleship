34d4b3ba5a5814b17e49a92b5063b14fa1b36863
aplyd <austin@ftacnik.com>
2020-04-06 19:35:33 -0700

    Updates

---
+++ precache-manifest.62688aa328d81cdf2ec38362b1e626ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bf98f1e7938d306c7f9a6498c0743d",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "5c252a64e15304310ea7",
+    "url": "/battleship/static/css/main.82d855cf.chunk.css"
+  },
+  {
+    "revision": "7bb368d7841b171375d1",
+    "url": "/battleship/static/js/2.3014d8f5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/battleship/static/js/2.3014d8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c252a64e15304310ea7",
+    "url": "/battleship/static/js/main.b675f9e4.chunk.js"
+  },
+  {
+    "revision": "267fa3ce54cc6279f95b",
+    "url": "/battleship/static/js/runtime-main.56b15e06.js"
+  }
+]);
